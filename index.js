3f2136c6dfdde1a46f763d05076d8aa4caa1e4ae
l1zondo <lizondofrancoezequiel@gmail.com>
2023-03-03 22:27:43 -0300

    proyecto terminado

--- index.js
+++ index.js
@@ -4,38 +4,37 @@ let coin = document.querySelector(".coin");
 let flipBtn = document.querySelector("#flip-button");
 let resetBtn = document.querySelector("#reset-button");
 
-flipBtn.addEventListener("click",()=>{
-    let i = Math.floor(Math.random()* 2);
+flipBtn.addEventListener("click", () => {
+    let i = Math.floor(Math.random() * 2);
     coin.style.animation = "none";
-    if(i){
-        setTimeout(function(){
+    if (i) {
+        setTimeout(function () {
             coin.style.animation = "spin-heads 3s forwards";
-        },100);
+        }, 100);
         heads++;
-    }else{
-        setTimeout(function(){
+    } else {
+        setTimeout(function () {
             coin.style.animation = "spin-tails 3s forwards";
-        },100);
+        }, 100);
         tails++;
     }
     setTimeout(updateStats, 3000);
     disableButton();
 });
 
-function updateStats(){
+function updateStats() {
     document.querySelector("#heads-count").textContent = `Heads: ${heads}`;
     document.querySelector("#tails-count").textContent = `Tails: ${tails}`;
 }
 
-function disableButton(){
+function disableButton() {
     flipBtn.disabled = true;
-    setTimeout(function(){
+    setTimeout(function () {
         flipBtn.disabled = false;
-    },3000);
+    }, 3000);
 }
 
-
-resetBtn.addEventListener("click",()=>{
+resetBtn.addEventListener("click", () => {
     coin.style.animation = "none";
     heads = 0;
     tails = 0;
